3e84f21db1d6ebfd4e10856688ca126b7b87e700
kevin1688 <jackexi@gmail.com>
2021-12-25 12:57:43 +0800

    Initial Commit

---
+++ OrderDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OrderDemo
+//
+//  Created by kai wen chen on 2021/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OrderDemo/OrderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderDemoApp.swift
+//  OrderDemo
+//
+//  Created by kai wen chen on 2021/12/25.
+//
+
+import SwiftUI
+
+@main
+struct OrderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
